bfbca35c090a1a4c5ee98119acc54d413b2186af
Richard Walker <digitalsadhu@gmail.com>
2014-04-12 17:27:57 +0200

    Update module to handle test spec

--- index.js
+++ index.js
@@ -3,10 +3,18 @@
 var through = require('through')
 
 var write = function write(data) {
-  var envArr = data.split('=')
+
+  data = data.toString('utf8')
+
+  //split on first occurance of =
+  var envArr = data.split(/=(.+)?/)
+
+  //clean up value, extracting from quotation if necessary
+  var val = envArr[1].replace(/^['"]/, '').replace(/['"]$/, '')
+
   var envObj = {
     key: envArr[0],
-    value: envArr[1]
+    value: val
   }
   this.emit('data', envObj)
 }
@@ -15,5 +23,7 @@ var end = function end () {
   this.emit('end')
 }
 
-module.exports = through(write, end)
+module.exports = function () {
+  return through(write, end)
+}
 
